ca3376cf371cf34ee08043f99393a13c319bda1c
cpravetz <chris@pravetz.net>
2022-08-09 16:58:03 -0400

    Eliminate redundant schemas for app

--- imports/api/schemas/componentDef.js
+++ imports/api/schemas/componentDef.js
@@ -2,13 +2,12 @@ import { Mongo } from 'meteor/mongo';
 import { BaseModel } from 'meteor/socialize:base-model';
 import SimpleSchema from 'simpl-schema';
 import './uiDef.js';
+import './buildDef.js';
 
 
 // Components that are assembled into the UI of an application are defined using the schemas
 // here in this file.  Builder uses these component definitions, an actions definition and the
 // application definition itself to generate the code/files of the desired application.
-//
-
 Schemas.propertySchema = new SimpleSchema({
     name : String,
     type : String,  //data type of the property
@@ -23,7 +22,7 @@ Schemas.propertySchema = new SimpleSchema({
 });
 
 Schemas.childRelationSchema = new SimpleSchema({
-    child : String,
+    childComponentName : String,
     required : {
         type: Boolean,
         defaultValue : true
@@ -35,54 +34,15 @@ Schemas.childRelationSchema = new SimpleSchema({
     order : Number
 });
 
-Schemas.componentSchema = new SImpleSchema({
+Schemas.componentSchema = new SimpleSchema({
     name : String,
     ancestor : String,
     children : Array,
     "children.$" : childRelationSchema,
     properties : Array,
-    "properties.$" : childRelationSchema,
-    //TODO Add the logic for rendering this component at design time and for pushing it into the code
-    // rules : Array,
-    // "rules.$" : ruleSchema,
-});
-
-Schemas.ruleSchema = new SimpleSchema({
-    command  : {
-        type: String,
-        allowedValues : ['create','execute','set','get','append','replace', 'if', 'each']
-    },
-    parameters : Array,
-    "parameter.$" : Object,
-    "parameter.$.name" : String,
-    "parameter.$.object" : String,
-    children : {
-        type : Array,
-        optional : true
-    },
-    "children.$" : {
-        type : Schemas.ruleSchema,
-    }
-});
-
-
-Schemas.appDefSchema = new SimpleSchema({
-    name : String,
-    version : Number,
+    "properties.$" : Schemas.childRelationSchema,
     steps : Array,
-    "steps.$" : Schemas.ruleSchema
+    "steps.$" : SimpleSchema.oneOf([Schemas.createSchema, Schemas.executeSchema,
+                                    Schemas.setSchema, Schemas.appendSchema,
+                                    Schemas,replaceSchema, Schemas.ifSchema, Schemas.eachSchema]);
 });
-
-
-// Rules...
-/*
-Create - folder
-       - file (from a template)
-Replace in file
-        in string (template field with property/variable)
-Set variable
-Get variable
-Append to file  - top
-                - bottom
-Execute command
-*/
